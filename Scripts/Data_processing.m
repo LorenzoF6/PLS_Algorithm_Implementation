845808935eb4dd628cbeb633f4c78ded748e2be5
Lorenzo Leoni <l.leoni2@studenti.unibg.it>
2023-03-17 09:12:31 +0100

    Changed dataset (to Iris)

--- Scripts/Data_processing.m
+++ Scripts/Data_processing.m
@@ -1,54 +1,78 @@
-
 clc
 clearvars
 warning off
 
-classes_DS = readtable("..\Data\Sources\Fault_classes_data.csv");
+%% Fault detection dataset
+
+faults_DS = readtable("..\Data\Sources\Fault_classes_data.csv");
 n = 6; % number of fault classes
-temp = array2table(zeros(size(classes_DS, 1), n));
+temp = array2table(zeros(size(faults_DS, 1), n));
 temp.Properties.VariableNames = {'class1', 'class2', 'class3', 'class4',...
     'class5', 'class6'};
-%% 
 
-for i = 1:size(classes_DS, 1)
+for i = 1:size(faults_DS, 1)
     % class 1: fault between phase A and ground
-    if classes_DS.G(i) == 1 && classes_DS.C(i) == 0 &&... 
-        classes_DS.B(i) == 0 && classes_DS.A(i) == 1
+    if faults_DS.G(i) == 1 && faults_DS.C(i) == 0 &&... 
+        faults_DS.B(i) == 0 && faults_DS.A(i) == 1
         temp.class1(i) = 1;
     end
 
     % class 2: fault between phases A, B and ground
-    if classes_DS.G(i) == 1 && classes_DS.C(i) == 0 &&... 
-        classes_DS.B(i) == 1 && classes_DS.A(i) == 1
+    if faults_DS.G(i) == 1 && faults_DS.C(i) == 0 &&... 
+        faults_DS.B(i) == 1 && faults_DS.A(i) == 1
         temp.class2(i) = 1;
     end
 
     % class 3: fault between phases B and C
-    if classes_DS.G(i) == 0 && classes_DS.C(i) == 1 &&... 
-        classes_DS.B(i) == 1 && classes_DS.A(i) == 0
+    if faults_DS.G(i) == 0 && faults_DS.C(i) == 1 &&... 
+        faults_DS.B(i) == 1 && faults_DS.A(i) == 0
         temp.class3(i) = 1;
     end
 
     % class 4: fault between all three phases
-    if classes_DS.G(i) == 0 && classes_DS.C(i) == 1 &&... 
-        classes_DS.B(i) == 1 && classes_DS.A(i) == 1
+    if faults_DS.G(i) == 0 && faults_DS.C(i) == 1 &&... 
+        faults_DS.B(i) == 1 && faults_DS.A(i) == 1
         temp.class4(i) = 1;
     end
 
     % class 5: three phases symmetrical fault 
-    if classes_DS.G(i) == 1 && classes_DS.C(i) == 1 &&... 
-        classes_DS.B(i) == 1 && classes_DS.A(i) == 1
+    if faults_DS.G(i) == 1 && faults_DS.C(i) == 1 &&... 
+        faults_DS.B(i) == 1 && faults_DS.A(i) == 1
         temp.class5(i) = 1;
     end
 
     % class 6: no fault
-    if classes_DS.G(i) == 0 && classes_DS.C(i) == 0 &&... 
-        classes_DS.B(i) == 0 && classes_DS.A(i) == 0
+    if faults_DS.G(i) == 0 && faults_DS.C(i) == 0 &&... 
+        faults_DS.B(i) == 0 && faults_DS.A(i) == 0
         temp.class6(i) = 1;
     end
 end
 
-classes_DS = removevars(classes_DS ,["A", "B", "C", "G"]);
-classes_DS = [temp classes_DS];
+faults_DS = removevars(faults_DS ,["A", "B", "C", "G"]);
+faults_DS = [temp faults_DS];
 
-% save("..\Data\Processed data\Classes_DS.mat", "classes_DS")
+save("..\Data\Processed data\Faults_DS.mat", "faults_DS")
+
+%% Iris dataset
+
+iris_DS = readtable("..\Data\Sources\IRIS.csv");
+n = 3; % number of fault classes
+temp = array2table(zeros(size(iris_DS, 1), n));
+temp.Properties.VariableNames = {'class1', 'class2', 'class3'};
+
+for i = 1:size(iris_DS, 1)
+    if strcmp(iris_DS.species{i}, 'Iris-setosa')
+        temp.class1(i) = 1;
+    end
+    if strcmp(iris_DS.species{i}, 'Iris-versicolor')
+        temp.class2(i) = 1;
+    end
+    if strcmp(iris_DS.species{i}, 'Iris-virginica')
+        temp.class3(i) = 1;
+    end
+end
+
+iris_DS = removevars(iris_DS ,["species"]);
+iris_DS = [temp iris_DS];
+
+save("..\Data\Processed data\Iris_DS.mat", "iris_DS")
--- Scripts/Temp.m
+++ Scripts/Temp.m
@@ -1,20 +1,22 @@
-
 clc
 clearvars
 warning off
 
-load ..\Data\'Processed data'\Classes_DS.mat
+%% Data analysis
 
-Y = table2array(classes_DS(:, 1:6));
-X = table2array(classes_DS(:, 7:end));
+load ..\Data\'Processed data'\Iris_DS.mat
 
-B = pls(X, Y);
-% B_2 = PLSI(normalize(X), normalize(Y));
-Y_hat = X*B;
+Y = table2array(iris_DS(:, 1:3));
+X = table2array(iris_DS(:, 4:end));
+
+B2 = pls(X, Y);
+Y_hat = normalize(X)*B2;
+scatter3(Y_hat(:,2), Y_hat(:,3), Y_hat(:,1))
+grid on
 
 for i = 1:size(Y_hat, 1)
     [~, j] = max(Y_hat(i, :));
-    for k = 1:6
+    for k = 1:3
         if k == j
             Y_hat(i, k) = 1;
         else
@@ -32,8 +34,9 @@ for i = 1: size(Y, 1)
     end
 end
 
-cont/size(Y, 1);
-%% 
+acc = 1 - cont/size(Y, 1);
+
+%% Function
 
 function B = pls(X, Y, mod2, maxIter, tol)
     
@@ -56,7 +59,7 @@ function B = pls(X, Y, mod2, maxIter, tol)
                 t = E*w; % j-th column of the score matrix for X
                 q = (F'*t)/norm(F'*t); % j-th column of the loading matrix for Y
                 u = F*q; % j-th column of the score matrix for Y
-                if tOld - t < tol
+                if abs(tOld - t) < tol
                     break;
                 else 
                     tOld = t;
@@ -93,7 +96,7 @@ function B = pls(X, Y, mod2, maxIter, tol)
             tol = 10e-10;
     end
     X = normalize(X);
-    % Y = normalize(Y);
+    Y = normalize(Y);
     if mod2
         B = pls2(X, Y, maxIter, tol);
     else
