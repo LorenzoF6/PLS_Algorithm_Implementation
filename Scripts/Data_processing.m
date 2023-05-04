053cb0bfd15223320fc2949c170cccc64088ef33
Lorenzo Leoni <l.leoni2@studenti.unibg.it>
2023-05-04 21:58:15 +0200

    Reorganized repository

--- Scripts/Data_processing.m
+++ Scripts/Data_processing.m
@@ -2,39 +2,7 @@ clc
 clearvars
 warning off
 
-%% Metal furnace dataset
-
-alloy_DS_2 = readtable("..\Data\Sources\Train.csv")
-
-n = 5; % number of fault classes
-temp = array2table(zeros(size(alloy_DS, 1), n));
-temp.Properties.VariableNames = {'class0', 'class1', 'class2', 'class3',...
-    'class4'};
-
-for i = 1:size(alloy_DS, 1)
-    if alloy_DS.grade(i) == 0
-        temp.class0(i) = 1;
-    end
-    if alloy_DS.grade(i) == 1
-        temp.class1(i) = 1;
-    end
-    if alloy_DS.grade(i) == 2
-        temp.class2(i) = 1;
-    end
-    if alloy_DS.grade(i) == 3
-        temp.class3(i) = 1;
-    end
-    if alloy_DS.grade(i) == 4
-        temp.class4(i) = 1;
-    end
-end
-
-alloy_DS = removevars(alloy_DS ,["grade", "f9"]);
-alloy_DS = [temp alloy_DS];
-
-save("..\Data\Processed data\Alloy_DS.mat", "alloy_DS")
-
-%% Iris dataset
+%% Iris dataset processing
 
 iris_DS = readtable("..\Data\Sources\IRIS.csv");
 n = 3; % number of fault classes
